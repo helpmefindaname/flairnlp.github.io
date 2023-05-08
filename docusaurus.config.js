3915bbfbfb991c355decd8fcde0e12dcfdc155c1
Benedikt Fuchs <benedikt.fuchs.staw@hotmail.com>
2023-05-08 11:20:15 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/flavicon.png',
 
   // Set the production url of your site here
-  url: 'https://flairnlp.github.io',
+  url: 'https://helpmefindaname.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
